9f0277a6a54a5ee80d7b0db42062e09e875a58c9
Carlos <carlosdanielpimentel@gmail.com>
2022-07-11 22:39:35 -0300

    Implementado validação de valor

--- src/pages/Fretes/ValorCarga.js
+++ src/pages/Fretes/ValorCarga.js
@@ -1,8 +1,16 @@
+import { useFocusEffect } from '@react-navigation/native';
 import React from 'react';
 import { Text, Image } from 'react-native';
 import styles from '../css/Styles';
 
 export default function ValorCarga({frete}){
+    //verificar se valor do frete é igual a 0    
+    var valorCarga = 1;
+    if(frete.valor == "0"){
+        valorCarga = "Á Combinar";
+    }else {
+        valorCarga = frete.valor;
+    }    
     return (
         <>
             <Text style={styles.titulosCard}>
@@ -10,8 +18,8 @@ export default function ValorCarga({frete}){
                     source={require('../../assets/icons/iconeValorCarga.png')}                                    
                 />
                 Valor R$
-            </Text>
-            <Text style={styles.textoCard}>{frete.valor}</Text>
+            </Text>            
+            <Text style={styles.textoCard}>{valorCarga}</Text>
         </>
     );
 }
